35821960fbba2e001faf873ebe7d01d0963c5ad9
Moe Ajam <mahmoud.othman.ajam@gmail.com>
2024-03-03 22:20:46 +0300

    Fixed the login bug.

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -38,39 +38,40 @@ function Login() {
 
     return (<div className="fixed inset-0 flex justify-center items-center bg-cover bg-center bg-no-repeat h-screen"
                  style={{backgroundImage: `url(${backgroundImage})`}}>
-            <Form className="flex flex-col bg-white w-152 shadow-lg p-14 rounded space-y-4">
-                <h1>Welcome To HeartLinks!</h1>
-                <input type="text" name="email" value={email} placeholder="Email"
-                       className="border border-gray-300 rounded pl-2"
-                       onChange={handleUsernameChange}/>
-                <input type="password" name="password" value={password} placeholder="Password"
-                       className="border border-gray-300 rounded pl-2"
-                       onChange={handlePasswordChange}/>
-                {errorMessage.isError && <p className="text-red-600">{errorMessage.message}</p>}
-                {navigation.state === "loading" && <Spinner color='green'/>}
-                <div className="flex self-end space-x-4">
-                    <button className={`flex items-center button-primary self-end ${isLoading ? 'bg-gray-500' : ''}`}
-                            disabled={isLoading || navigation.state === 'loading'}>
-                        {isLoading ? 'Loading' : 'Login'}
-                        {isLoading && <Spinner/>}
-                    </button>
-                    <button className="flex items-center button-secondary self-end" onClick={onRegister}
-                            disabled={isLoading || navigation.state === "loading"}>
-                        Register
-                    </button>
-                </div>
-            </Form>
-        </div>);
+        <Form method='post' className="flex flex-col bg-white w-152 shadow-lg p-14 rounded space-y-4">
+            <h1>Welcome To HeartLinks!</h1>
+            <input type="text" name="email" value={email} placeholder="Email"
+                   className="border border-gray-300 rounded pl-2"
+                   onChange={handleUsernameChange}/>
+            <input type="password" name="password" value={password} placeholder="Password"
+                   className="border border-gray-300 rounded pl-2"
+                   onChange={handlePasswordChange}/>
+            {errorMessage.isError && <p className="text-red-600">{errorMessage.message}</p>}
+            {navigation.state === "loading" && <Spinner color='green'/>}
+            <div className="flex self-end space-x-4">
+                <button value="login"
+                        className={`flex items-center button-primary self-end ${isLoading ? 'bg-gray-500' : ''}`}
+                        disabled={isLoading || navigation.state === 'loading'}>
+                    {isLoading ? 'Loading' : 'Login'}
+                    {isLoading && <Spinner/>}
+                </button>
+                <button className="flex items-center button-secondary self-end" onClick={onRegister}
+                        disabled={isLoading || navigation.state === "loading"}>
+                    Register
+                </button>
+            </div>
+        </Form>
+    </div>);
 }
 
 export default Login;
 
 
-// todo: the action is not being called on login button press.
-export async function action({request, params}) {
+export async function action({request}) {
+    const data = await request.formData();
 
     const loginData = {
-        email: request.email, password: request.password,
+        email: data.get('email'), password: data.get('password'),
     }
 
     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/login`, {
@@ -78,6 +79,8 @@ export async function action({request, params}) {
             'Content-Type': 'application/json',
         }, body: JSON.stringify(loginData)
     });
+
+    console.log(response);
 
     if (response.status === 401) {
         throw json({message: 'Wrong username or password.'}, {status: 401});
